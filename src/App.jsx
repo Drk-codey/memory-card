04c15e56cc5b7a8025d3ba578edb668e0d2817e4
Drk~Codey <109389152+Drk-codey@users.noreply.github.com>
2023-11-02 01:11:03 +0100

    Add the Top-level and entry file with thier styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import CardGrid from './components/CardGrid.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards , setCards] = useState([]); //Array of cards
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [selectedCards, setSelectedCards] = useState([]);
+
+  useEffect(() => {
+    initializeGame(); //Initialize the game
+  }, []);
+
+  const  initializeGame = () => {
+    const initialCards = [
+      {id: 1, name: "Naruto Uzumaki", image: "/src/assets/naruto-Uzumaki.jpg"},
+      {id: 2, name: "Kakashi Hatake", image: "src/assets/Kakashi Hatake.jpg"},
+      {id: 3, name: "Lady Tsunade", image: "src/assets/Lady Tsunade.jpg"},
+      {id: 4, name: "Shisui-Uchiha", image: "src/assets/Shisui-Uchiha.jpg"},
+      {id: 5, name: "Sasuke Uchiha", image: "src/assets/Sasuke Uchiha.jpg"},
+      {id: 6, name: "Shikamaru Nara", image: "src/assets/Shikamaru Nara.jpg"},
+      {id: 7, name: "Isshiki Ōtsutsuki", image: "src/assets/Isshiki Ōtsutsuki.jpg"},
+      {id: 8, name: "Nagato", image: "src/assets/Nagato.jpg"},
+      {id: 9, name: "Might-Guy", image: "src/assets/Might-Guy.jpg"},
+      {id: 10, name: "Code", image: "src/assets/Code.jpg"},
+    ];
+
+    setCards(shuffle(initialCards));
+  };
+
+  // Fisher-Yates Shuffle Algorithm for the cards
+  const shuffle = (array) => {
+    // Loop through array starting at the last index
+    for (let i = array.length - 1; i > 0; i--) {
+      // Generate a random index from 0 - i
+      const j = Math.floor(Math.random() * (i + 1));
+
+      // Swap elements at indexes i and j
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  const handleCardClick = (selectedCard) => {
+    // Check if card has already been selected
+    if (selectedCard.selected || selectedCards.includes(selectedCard.id)) {
+      console.log(`Please don't click me again!`, selectedCard.selected);
+      // Handle when player clicks the same card twice
+      handleWrongSelection();
+    } else {
+      // Update the selected card"s state
+      const updateCards = cards.map((card) => {
+        if (card === selectedCard) {
+          console.log(selectedCard.selected);
+          return {...card, selected: true};
+        }
+        return card;
+      });
+
+      setCards(updateCards);
+      setSelectedCards([...selectedCards, selectedCard.id]);
+      setScore(score + 1);
+      if (score >= 10) {
+        setBestScore(score);
+        setScore(0);
+      }
+    }
+
+    setCards(shuffle(cards));
+  };
+
+  const handleWrongSelection = () => {
+    setSelectedCards([]);
+    handleBestScore();
+    setScore(0);
+    initializeGame();
+  };
+
+  const handleBestScore = () => {
+    if (score > bestScore) {
+      setBestScore(score);
+    }
+    bestScore
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header score={score} bestScore={bestScore} />
+      <CardGrid cards={cards} onCardClick={handleCardClick} />
     </>
   )
 }
